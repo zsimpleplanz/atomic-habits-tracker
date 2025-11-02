84e899ff3550ef223294edf3815c3df524f8ab9c
zsimpleplanz <52979526+zsimpleplanz@users.noreply.github.com>
2025-11-02 11:31:37 +0700

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'atomic-habits-v1';
+const urlsToCache = [
+  '/atomic-habits-tracker/',
+  '/atomic-habits-tracker/index.html',
+  '/atomic-habits-tracker/manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.log('Cache failed:', err))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(err => fetch(event.request))
+  );
+});
